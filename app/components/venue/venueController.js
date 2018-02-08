e68fc4c4d6044ac821e565b6b25bdfdef17e45a8
Nicholas Decker <nickdeckerdevs@gmail.com>
2018-02-08 13:50:14 -0500

    removed some test code

--- app/components/venue/venueController.js
+++ app/components/venue/venueController.js
@@ -1,12 +1,11 @@
 angular.module('venue.controller', [])
 .controller('venueController', function($scope, yelpAPIservice, storageService) {
 	$scope.dynamicVenueList = {};
-	$scope.test = 'not loaded';
-	$scope.filters = {};
+
+	scope.filters = {};
 	$scope.getMyVenues = function() {
 		$scope.filters = storageService.getApiData();
 		yelpAPIservice.getVenues($scope).success(function(response) {
-			$scope.test = 'loadedAF';
 			console.log(response);
 			$scope.dynamicVenueList = response.businesses;
 		});
