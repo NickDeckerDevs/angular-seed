692e44bb78e4d689a2157e0945dc046e41bb2b4d
Nicholas Decker <nickdeckerdevs@gmail.com>
2018-02-08 16:27:22 -0500

    upgraded angular and lost success() - had to change to then()

--- app/components/venue/venueController.js
+++ app/components/venue/venueController.js
@@ -1,13 +1,21 @@
 angular.module('venue.controller', [])
 .controller('venueController', function($scope, yelpAPIservice, storageService) {
 	$scope.dynamicVenueList = {};
-
-	scope.filters = {};
+	$scope.filters = {};
 	$scope.getMyVenues = function() {
 		$scope.filters = storageService.getApiData();
-		yelpAPIservice.getVenues($scope).success(function(response) {
-			console.log(response);
-			$scope.dynamicVenueList = response.businesses;
+		yelpAPIservice.getVenues($scope)
+			.then(function onSuccess(response) {
+				console.log(response.data);
+				$scope.dynamicVenueList = response.data.businesses;
+			}).catch(function onError(response) {
+				console.log('error!')
+				var data = response.data;
+				console.log(data)
+			    var status = response.status;
+			    var statusText = response.statusText;
+			    var headers = response.headers;
+			    var config = response.config;
 		});
 	}
 });
