ed573bb77ac936244dbd96fe666926117cb24f52
Nicholas Decker <nickdeckerdevs@gmail.com>
2018-02-08 11:00:49 -0500

    starting bring in data from local storage on init

--- app/components/user/userController.js
+++ app/components/user/userController.js
@@ -1,16 +1,15 @@
 angular.module('user.controller', [])
 .controller('preferencesController', function($scope, geolocationservice, storageService) {
-	$scope.locationFilter = 'Ft. Myers, FL';
-	$scope.latitudeFilter = '';
-	$scope.longitudeFilter = '';
+	console.log(storageService.getAllData())
+	$scope.locationFilter = storageService.getData('locationFilter');
+	$scope.latitudeFilter = storageService.getData('latitudeFilter');
+	$scope.longitudeFilter = storageService.getData('longitudeFilter');
 	$scope.sortOptions = ['rating', 'best_match', 'review_count'];
-	$scope.sortFilter = 'rating';
-	$scope.limitFilter = 30;
+	$scope.sortFilter = storageService.getData('sortFilter');
 	$scope.dynamicVenueList = [];
-	$scope.distanceFilter = '5';
+	$scope.distanceFilter = parseInt(1609.34 * 5).toString();
 	$scope.distanceOptions = ['5', '7', '10', '12', '15'];
-
-	$scope.usingLocation = false;
+	$scope.usingLocation = storageService.getData('usingLocation');
 	$scope.locationType = $scope.usingLocation == false ? 'Address' : 'Location';
 	$scope.isAddress = function() {
 		return $scope.locationType == 'address' ? true : false;
@@ -24,10 +23,12 @@ angular.module('user.controller', [])
 	$scope.getCurrentLocation = function() {
 		$scope.locationFilter = '';
 		$scope.usingLocation = true;
+		// start loading
 		geolocationservice.getCurrentPosition().then(function(response) {
 			$scope.latitudeFilter = response.latitude;
 			$scope.longitudeFilter = response.longitude;
 			$scope.locationType = 'Current Location';
+			//end loading
 		});
 	}
 	// ng-change methods
