35d74d3b896eacde4e3bb7ffbfed16164f212c20
Nicholas Decker <nickdeckerdevs@gmail.com>
2018-02-08 16:28:16 -0500

    Thinking about doing custom directive then changing mind

--- app/shared/directives.js
+++ app/shared/directives.js
@@ -1,15 +1,19 @@
-// angular.module('app.directives', [])
-// .directive('locationButton', function() {
-// 	// return {
-// 	// 	restrict: 'E',
-// 	// 	template: '<input id="{{ id }}" name="{{ name }}" type="button" value="{{ value }}" ng-click="{{ method }}">',
-// 	// 	replace: true,
-// 	// 	scope: {},
-// 	// 	link: function($scope, element, attrs) {
-// 	// 		element.on('click', function(e) {
-// 	// 			console.log(element)
-// 	// 			console.log($scope)
-// 	// 		});
-// 	// 	}
+angular.module('app.directives', [])
+// .directive('stars', function() {
+// 	return {
+// 		var starsHtml = '';
+// 		var full = '<i class="fas fa-star"></i>';
+// 		var half = '<i class="fas fa-star-half"></i>';
+// 		var empty = '<i class="far fa-star"></i>';
+// 		for(var i = 1; i <= 5; i++) {
+// 			if(i <= totalStars) {
+// 				starsHtml += full;
+// 			} else if(i > totalStars && (totalStars - .5) == i) {
+// 				starsHtml += half;
+// 			} else {
+// 				starsHtml += empty;
+// 			}
+// 		}
+// 		return starsHtml.trustAs(type || 'html', );
 // 	}
 // })
